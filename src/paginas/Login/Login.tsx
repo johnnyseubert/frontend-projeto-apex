b351ff50e1e55b61a564068e2e706581f13568d3
luizrein <luizreinprog@gmail.com>
2023-01-27 21:59:14 -0300

    html e css pagina login + css index

--- src/paginas/Login/Login.tsx
+++ src/paginas/Login/Login.tsx
@@ -6,7 +6,19 @@ export const PaginaLogin = () => {
 		<div className="pagina_login">
 			<div className="container_login">
 				<div className='formulario_login'>
-					
+					<div className='formulario_topo'>
+						<h1>Entrar</h1>
+						<img src="/imagens/logo.png"/>
+					</div>
+					<div className='formulario_corpo'>
+						<input type="text" className='input' placeholder='email'/>
+						<input type="text" className='input' placeholder='senha'/>
+						<div className='entrar_linha'>
+							<button className='btn'>Entrar</button>
+							<a className='link' href="/esqueci_senha">Esqueci a senha</a>
+						</div>
+						<a className='btn' href="/cadastro">Cadastrar-se</a>
+					</div>
 				</div>
 				<div className='imagem_login'>
 					<img src="/imagens/login.jpg"/>
